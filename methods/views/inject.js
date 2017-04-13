702d97700f47a833ef839f1ad2eb9b88838844f8
Chris <orthagonal1@gmail.com>
2017-04-13 13:00:28 -0500

    pass user agent and referrer

--- methods/views/inject.js
+++ methods/views/inject.js
@@ -1,8 +1,10 @@
 'use strict';
 const Boom = require('boom');
+const version = require('../../package.json').version;
 
 module.exports = (request, url, allDone) => {
-  request.server.inject({ url }, (response) => {
+  const headers = { referer: request.info.referrer, 'user-agent': `hapi-views/${version}` };
+  request.server.inject({ url, headers }, (response) => {
     if (response.statusCode !== 200) {
       return allDone(Boom.create(response.statusCode, response.statusMessage));
     }
