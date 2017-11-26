5501c21e4b4026b09c7f3e122654bda43f732332
orthagonal <orthagonal1@gmail.com>
2017-11-26 01:16:00 -0600

    remove comments

--- methods/views/yaml.js
+++ methods/views/yaml.js
@@ -4,22 +4,8 @@ const fs = require('fs');
 
 module.exports = async (yamlFile) => {
   return new Promise((resolve, reject) => {
-    /// why this no load?
-    /// why this no load?
-    /// why this no load?
-    /// why this no load?
-    /// why this no load? path is correct
-    console.log(yamlFile)
-    // this is a placeholder:
-    // this is a placeholder:
-    // this is a placeholder:
     return resolve({ test1: true });
     fs.readFile(yamlFile, 'utf8', (err, contents) => {
-      console.log('+')
-      console.log('+')
-      console.log('+')
-      console.log(err)
-      console.log(contents)
       if (err) {
         return reject(err);
       }
